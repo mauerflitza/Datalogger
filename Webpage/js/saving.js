13cb02f8d28ecec47550e833883152fd9d22a7e4
mauerflitza <tobias.friedrich1@gmx.de>
2016-08-11 11:34:21 +0900

    Update saving.js

--- Webpage/js/saving.js
+++ Webpage/js/saving.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
     // process the form
-    $('form').submit(function(event) {
+    $('#Save').submit(function(event) {
 
         // get the form data
         // there are many ways to get this data using jQuery (you can use the class or id also)
