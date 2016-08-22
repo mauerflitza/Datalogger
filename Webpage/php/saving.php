de5612581dca0c0ec00d270a183ce5325166ab5e
mauerflitza <tobias.friedrich1@gmx.de>
2016-08-22 14:59:19 +0900

    Update saving.php

--- Webpage/php/saving.php
+++ Webpage/php/saving.php
@@ -1,13 +1,7 @@
 <?php
-// ajax save.php
-$errors		= array();	//array to hold validation errors
 $data		= array();	//array to pass data back
-// validate the variables ========================================
-	// if any of these variables don't exist, add an error to our $errors array
-	//+++++++++++++++++++++++++++++++++++++++++
-// return a response ==============================================
-	// if there are any errors aray, return a success boolean of false
-	// DO ALL YOUR FORM PROCESSING HERE
+
+	//Saves the Webpage with all necassery data to a file
 	$savings = fopen('/home/pi/datalogger/loggerconfigs/savings.txt','w');
 	fwrite($savings,"StartValues:");
 	fwrite($savings,$_POST['StartVal'] . "/" . $_POST['selected'] . "/" . "\n");
@@ -15,8 +9,8 @@ $data		= array();	//array to pass data back
 	fwrite($savings,$_POST['SampleRates'] . "\n");
 	fwrite($savings,"html:");
 	fwrite($savings,$_POST['webpage'] . "\n");
-
 	fclose($savings);
+	
 	//show a message of success and provide a true success variable
 	$data['success'] = true;
 	$data['message'] = 'Success!';
