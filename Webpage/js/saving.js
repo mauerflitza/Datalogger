ad1a9f1cdac1f1443b7564dcfec3e5bcabbb0f29
mauerflitza <tobias.friedrich1@gmx.de>
2016-08-22 14:52:28 +0900

    Update saving.js

--- Webpage/js/saving.js
+++ Webpage/js/saving.js
@@ -1,15 +1,15 @@
-// magic.js
+// saving.js
+//*************************************************
+//Function to send the Saved Data to the Server with a Ajax request
+//*************************************************
 $(document).ready(function() {
 
     // process the form
     $('#Save').submit(function(event) {
 
-        // get the form data
-        // there are many ways to get this data using jQuery (you can use the class or id also)
-
         // process the form
         $.ajax({
-            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            type        : 'POST', // tzpe of HTTP verp we use
             url         : '../php/saving.php', // the url where we want to POST
             data        : $("#Save").serialize(), // our data object
             dataType    : 'json', // what type of data do we expect back from the server
@@ -21,8 +21,6 @@ $(document).ready(function() {
                 // log data to the console so we can see
 		alert("Setup saved");
                 console.log(data); 
-
-                // here we will handle errors and validation messages
             });
         // stop the form from submitting the normal way and refreshing the page
         event.preventDefault();
